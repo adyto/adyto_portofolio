9ceaf754c9ec303e4eebeea9e0532f77f8f94363
ady.to <adiyulianto61@gmail.com>
2023-05-31 08:17:18 +0700

    resume

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       </ul>
       <div className="max-md:hidden ">
         <a
-          href="https://www.cakeresume.com/pdf/s--oL4v67sW8-7gggwKQMg8iQ--/rQkAW.pdf"
+          href="https://www.cakeresume.com/pdf/s--6uGkmdA91duVZEGEOoZG1w--/pJV5j.pdf"
           className="flex flex-row items-center w-full h-full gap-2 px-3 py-1 text-white rounded-full cursor-pointer bg-color-palette-2/80"
         >
           <AiOutlineCloudDownload />
@@ -71,7 +71,7 @@ const Navbar = () => {
                 </li>
               ))}
               <a
-                href="https://www.cakeresume.com/pdf/s--oL4v67sW8-7gggwKQMg8iQ--/rQkAW.pdf"
+                href="https://www.cakeresume.com/pdf/s--6uGkmdA91duVZEGEOoZG1w--/pJV5j.pdf"
                 className="flex flex-row items-center gap-2 px-3 py-1 text-white rounded-full cursor-pointer bg-color-palette-2/80"
               >
                 <AiOutlineCloudDownload />
